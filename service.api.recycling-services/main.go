da46ac2cbe49be588a338481f9c7bfff358a53b0
Edward Stell <edward.j.stell@gmail.com>
2021-01-02 17:32:58 +0000

    fixed wiring to use new handler

--- service.api.recycling-services/main.go
+++ service.api.recycling-services/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/edstell/lambda/api"
 	"github.com/edstell/lambda/service.api.recycling-services/domain"
+	"github.com/edstell/lambda/service.api.recycling-services/handler"
+	"github.com/edstell/lambda/service.api.recycling-services/store"
 )
 
 func timeNowUTC() time.Time {
@@ -13,7 +19,17 @@ func timeNowUTC() time.Time {
 }
 
 func main() {
+	region := os.Getenv("AWS_REGION")
+	store := store.NewDynamoDB(
+		dynamodb.New(
+			session.New(),
+			aws.NewConfig().WithRegion(region),
+		),
+		timeNowUTC,
+	)
+	bl := domain.NewBusinessLogic(store)
 	router := api.NewRouter()
-	router.Route("GET", "/properties/{property}", domain.ReadProperty(nil))
+	router.Route("GET", "/properties/{property}", handler.GETProperty(bl))
+	router.Route("PUT", "/properties/{property}", handler.PUTProperty(bl, timeNowUTC))
 	lambda.Start(router.Handler)
 }
