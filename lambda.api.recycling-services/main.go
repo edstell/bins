b76c1c6e51d04bd38e41931a4ac8e92246d27498
Edward Stell <edward.j.stell@gmail.com>
2021-01-03 17:54:24 +0000

    added instrumentation to the lambda client

--- lambda.api.recycling-services/main.go
+++ lambda.api.recycling-services/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	svc "github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/edstell/lambda/lambda.api.recycling-services/domain"
 	"github.com/edstell/lambda/lambda.api.recycling-services/handler"
 	recyclingservices "github.com/edstell/lambda/lambda.recycling-services/rpc"
@@ -17,7 +18,10 @@ func main() {
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(os.Getenv("AWS_REGION")),
 	}))
-	logic := domain.NewBusinessLogic(recyclingservices.NewClient(svc.New(sess)))
+	lambdaService := svc.New(sess)
+	// Instrument the lambda client.
+	xray.AWS(lambdaService.Client)
+	logic := domain.NewBusinessLogic(recyclingservices.NewClient(lambdaService))
 	router := api.NewRouter()
 	router.Route("GET", "/properties/{property}", handler.GETProperty(logic))
 	lambda.Start(router.Handler)
